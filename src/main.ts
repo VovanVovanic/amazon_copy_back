bf11b5f7c46bc4887b2ba1a275f6b36b378c034d
Vladimir <vladimirplotnikov371@gmil.om>
2023-09-13 16:28:56 +0300

    env

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { PrismaService } from './prisma.service';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { PrismaService } from "./prisma.service";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
+  const PORT = process.env.PORT || 4200
   await prismaService.enableShutdownHooks(app);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
   app.enableCors();
-  await app.listen(4200);
+  await app.listen(PORT);
 }
 bootstrap();
